ff72fa434233c0305e50764a66f8159e93428cc9
Rashad Gasimzada <rashad.gasimzade.2000@gmail.com>
2021-12-15 21:53:58 +0400

    Tasks about delegates created

---
+++ Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Delegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(OddSum(IsOdd,5,10));
+            Console.WriteLine(OddSum(i => i % 2 != 0,5,10));
+
+            Func<string, string> print = StrNameInsert;
+            Console.WriteLine(print("Gasimzada"));
+
+            Action<string, string> words = FullName;
+            words("Hikmet","Hikmetov");
+            words("Rovshen", "Agayev");
+        }
+
+        public static bool IsOdd(int i)
+        {
+            return i % 2 != 0;
+        }
+        // Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapan metod
+        public static int OddSum(Predicate<int>func,int n,int m)
+        {
+            int sum = 0;
+            if (n < 1 || m < 1)
+            {
+                Console.WriteLine("Enter the correct number");
+            }
+            else
+            {
+                for (int i = n; i < m; i++)
+                {
+                    if (func(i))
+                    {
+                        sum += i;
+                    }
+                }
+            }
+            return sum;
+        }
+        public static string StrNameInsert(string str)
+        {
+            return str.Insert(0,"Rashad ");
+        }
+        public static void FullName (string name,string surname)
+        {
+            Console.WriteLine($"{name} {surname}");
+        }
+    }
+}
